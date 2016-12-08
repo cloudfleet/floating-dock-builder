9f5146fa02f1c2e5bc60cff64db3e716773f4603
Christoph Witzany <christoph@web.crofting.com>
2016-12-08 19:57:57 +0100

    small fixes

--- setup-raspbian-builder.sh
+++ setup-raspbian-builder.sh
@@ -2,7 +2,7 @@
 
 BUILDER_ADDRESS=$1
 FLOATING_DOCK_ADDRESS=$2
-FLOATING_DOCK_NEW_BUILDER_KEY=$2
+FLOATING_DOCK_NEW_BUILDER_KEY=$3
 BUILDER_PREFIX=$4
 BUILDER_NUMBER=$5
 
@@ -10,8 +10,9 @@ LOGIN_USER=pi
 
 NEW_HOSTNAME=${BUILDER_PREFIX}${BUILDER_NUMBER}
 SCRIPTS_DIR=/opt/floating-dock/builder
+CONFIG_DIR=/etc/floating-dock/builder
 
-SCRIPT_FILE=/tmp/floating-dock-${FLOATING_DOCK_ADDRESS}-${BUILDER_ADDRESS}-script
+SCRIPT_FILE=/tmp/floating-dock-${NEW_HOSTNAME}-script
 
 
 cat > ${SCRIPT_FILE} <<SCRIPT
@@ -29,15 +30,17 @@ sed -i s/\$(hostname)/${NEW_HOSTNAME}/g /etc/hostname
 hostname ${NEW_HOSTNAME}
 
 
+mkdir -p ${CONFIG_DIR}
+
 mkdir -p ${SCRIPTS_DIR}
 cd ${SCRIPTS_DIR}
 
-wget https://${FLOATING_DOCK_ADDRESS}/builders/scripts.tar.gz
+wget ${FLOATING_DOCK_ADDRESS}/api/v1/builders/scripts.tar.gz
 tar -xzf scripts.tar.gz
 
 
 echo "#!/bin/bash" > /etc/rc.local
-echo "nohup python ${SCRIPTS_DIR}/build_runner.py ${FLOATING_DOCK_ADDRESS} ${FLOATING_DOCK_NEW_BUILDER_KEY} >> /var/log/floating_dock_runner.log &" >> /etc/rc.local
+echo "nohup python -u ${SCRIPTS_DIR}/build_runner.py ${FLOATING_DOCK_ADDRESS} ${FLOATING_DOCK_NEW_BUILDER_KEY} ${NEW_HOSTNAME} >> /var/log/floating_dock_runner.log 2> &1 &" >> /etc/rc.local
 chmod +x /etc/rc.local
 /etc/rc.local
 
