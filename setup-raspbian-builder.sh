532f108dd9283cd517c737abb45620aba21a8d74
Christoph Witzany <christoph@web.crofting.com>
2017-05-27 08:37:47 +0200

    Add builder architecture and make it possible to install on aarch64

--- setup-raspbian-builder.sh
+++ setup-raspbian-builder.sh
@@ -5,6 +5,7 @@ FLOATING_DOCK_ADDRESS=$2
 FLOATING_DOCK_NEW_BUILDER_KEY=$3
 BUILDER_PREFIX=$4
 BUILDER_NUMBER=$5
+BUILDER_ARCHITECTURE=$6
 
 LOGIN_USER=pi
 
@@ -20,7 +21,7 @@ cat > ${SCRIPT_FILE} <<SCRIPT
 sudo su -
 
 echo "Installing dependencies"
-wget -nv -O - https://get.docker.com/ | sh
+wget -nv -O - https://get.docker.com/ | sh || (apt update && apt -y install docker.io)
 echo "Setting Hostname to ${NEW_HOSTNAME}"
 
 sed -i s/\$(hostname)/${NEW_HOSTNAME}/g /etc/hosts
@@ -40,10 +41,11 @@ tar -xzf scripts.tar.gz
 
 
 echo "#!/bin/bash" > /etc/rc.local
-echo "nohup python -u ${SCRIPTS_DIR}/build_runner.py ${FLOATING_DOCK_ADDRESS} ${FLOATING_DOCK_NEW_BUILDER_KEY} ${NEW_HOSTNAME} >> /var/log/floating_dock_runner.log 2>&1 &" >> /etc/rc.local
+echo "nohup python -u ${SCRIPTS_DIR}/build_runner.py ${FLOATING_DOCK_ADDRESS} ${FLOATING_DOCK_NEW_BUILDER_KEY} ${NEW_HOSTNAME} ${BUILDER_ARCHITECTURE} >> /var/log/floating_dock_runner.log 2>&1 &" >> /etc/rc.local
 chmod +x /etc/rc.local
 /etc/rc.local
 
 SCRIPT
 
 ssh $LOGIN_USER@$BUILDER_ADDRESS < ${SCRIPT_FILE}
+rm ${SCRIPT_FILE}
